3089603d821f276e7326585cafdef550ea7880df
AndreiTapit <andrewsavenkov68@gmail.com>
2026-02-25 13:52:00 +0300

    Add files via upload

---
+++ moduletwo/moduletwo/db/createTBfilms.sql
@@ -0,0 +1,12 @@
+CREATE TABLE films(
+	sname_film TEXT NOT NULL,
+	description VARCHAR(254) ,
+	date_film  DATE,
+	genre TEXT,
+
+
+
+
+
+
+)
---
+++ moduletwo/moduletwo/db/createTBusers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users(
+	id INT NOT NULL ,
+	name VARCHAR(10) NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+)
