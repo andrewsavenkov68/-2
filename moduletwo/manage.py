c755f31cae60b82aeaa22a48e193cb66bcb79a9f
AndreiTapit <andrewsavenkov68@gmail.com>
2026-02-25 14:17:56 +0300

    Add files via upload

--- moduletwo/manage.py
+++ moduletwo/manage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 import sqlite3
 connection = sqlite3.connect("localdb")
+users = User.objects.all()
+
+
+
+
+
+
 
---
+++ moduletwo/moduletwo/db/SQLFile2.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'Ivan', 'ivan@mail.com', 'qwerty123');
